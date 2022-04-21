ed1ac2509059931ffee198e75a92a0bf9cdd42a9
y-lifeReview <1847868423@qq.com>
2022-04-21 18:34:09 +0800

    feat '新增错误日志'

--- node/backend/back/app.js
+++ node/backend/back/app.js
@@ -2,7 +2,8 @@ var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+var morgan = require('morgan');
+const logger = require('./logger')
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -13,7 +14,7 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-app.use(logger('dev'));
+app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
@@ -28,14 +29,39 @@ app.use(function(req, res, next) {
 });
 
 // error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+// app.use(function(err, req, res, next) {
+//   // set locals, only providing error in development
+//   res.locals.message = err.message;
+//   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+//   // render the error page
+//   res.status(err.status || 500);
+//   res.render('error');
+// });
+
+const _errorHandel = (err,req,res,next)=>{
+  logger.error(`${req.method} ${req.originaUrl}` + err.message);
+  const errMsg = err.message;
+  res.status(err.status || 500).json({
+    code:-1,
+    success:false,
+    message:errMsg,
+    data:{}
+  })
+}
+app.use(_errorHandel)
+//设置跨域访问
+app.all("*", function (req, res, next) {
+	//设置允许跨域的域名，*代表允许任意域名跨域
+	res.header("Access-Control-Allow-Origin", "*");
+	//允许的header类型
+	res.header("Access-Control-Allow-Headers", "content-type");
+	//跨域允许的请求方式 
+	res.header("Access-Control-Allow-Methods", "DELETE,PUT,POST,GET,OPTIONS");
+	if (req.method == 'OPTIONS')
+		res.sendStatus(200); //让options尝试请求快速结束
+	else
+		next();
 });
 
 module.exports = app;
