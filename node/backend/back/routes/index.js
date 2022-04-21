ae63df7fd59125d174d0a5b58528bf7befd40fde
y-lifeReview <1847868423@qq.com>
2022-04-21 18:05:15 +0800

    feat 'router'

--- node/backend/back/routes/index.js
+++ node/backend/back/routes/index.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  // res.send('message')
   res.render('index', { title: 'Express' });
 });
 
+router.get('/user2', function(req, res, next) {
+  res.send('message')
+});
 module.exports = router;
