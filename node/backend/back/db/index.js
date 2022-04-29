013257a0a4d645b0f784cb6c1828bfb4f5883830
y-lifeReview <1847868423@qq.com>
2022-04-29 17:20:51 +0800

    feat:hide password

--- node/backend/back/db/index.js
+++ node/backend/back/db/index.js
@@ -2,7 +2,7 @@ let mysql =require('mysql')
 var connection = mysql.createConnection({
     host     : 'gz-cynosdbmysql-grp-pd4ndjr1.sql.tencentcdb.com',
     user     : 'root',
-    password : '19980508cX',
+    password : '',
     port:'27251',
     database : 'test'
   });
